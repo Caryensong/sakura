53d45e584993fceba8dcb186d46053a575dbd7e0
Caryensong <caryensong@googlemail.com>
2025-02-11 16:47:45 +0100

    update scss-function

--- src/app/main-content/landingPage/landingPage.component.ts
+++ src/app/main-content/landingPage/landingPage.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 
 
 @Component({
     selector:'app-landingPage',
     standalone: true,
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [ NavbarComponent],
     template: `
     <section>
      <app-navbar></app-navbar>
